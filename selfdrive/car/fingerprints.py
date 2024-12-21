8479bfc04ecc51ec009815a7c28712d151747e75
Kevin Martinez <kevinvr@hotmail.es>
2024-12-21 04:54:06 -0600

    #3 Fixed: Missing values for drone port

--- selfdrive/car/fingerprints.py
+++ selfdrive/car/fingerprints.py
@@ -1,5 +1,6 @@
 from openpilot.selfdrive.car.interfaces import get_interface_attr
 from openpilot.selfdrive.car.body.values import CAR as BODY
+from openpilot.selfdrive.car.drone.values import CAR as DRONE
 from openpilot.selfdrive.car.chrysler.values import CAR as CHRYSLER
 from openpilot.selfdrive.car.ford.values import CAR as FORD
 from openpilot.selfdrive.car.gm.values import CAR as GM
@@ -130,6 +131,7 @@ MIGRATION = {
 
   # Removal of platform_str, see https://github.com/commaai/openpilot/pull/31868/
   "COMMA BODY": BODY.COMMA_BODY,
+  "DRONE": DRONE.DRONE,
   "CHRYSLER PACIFICA HYBRID 2017": CHRYSLER.CHRYSLER_PACIFICA_2017_HYBRID,
   "CHRYSLER PACIFICA HYBRID 2018": CHRYSLER.CHRYSLER_PACIFICA_2018_HYBRID,
   "CHRYSLER PACIFICA HYBRID 2019": CHRYSLER.CHRYSLER_PACIFICA_2019_HYBRID,
--- selfdrive/car/values.py
+++ selfdrive/car/values.py
@@ -1,5 +1,6 @@
 from typing import get_args
 from openpilot.selfdrive.car.body.values import CAR as BODY
+from openpilot.selfdrive.car.drone.values import CAR as DRONE
 from openpilot.selfdrive.car.chrysler.values import CAR as CHRYSLER
 from openpilot.selfdrive.car.ford.values import CAR as FORD
 from openpilot.selfdrive.car.gm.values import CAR as GM
@@ -13,7 +14,7 @@ from openpilot.selfdrive.car.tesla.values import CAR as TESLA
 from openpilot.selfdrive.car.toyota.values import CAR as TOYOTA
 from openpilot.selfdrive.car.volkswagen.values import CAR as VOLKSWAGEN
 
-Platform = BODY | CHRYSLER | FORD | GM | HONDA | HYUNDAI | MAZDA | MOCK | NISSAN | SUBARU | TESLA | TOYOTA | VOLKSWAGEN
+Platform = BODY | CHRYSLER | FORD | GM | HONDA | HYUNDAI | MAZDA | MOCK | NISSAN | SUBARU | TESLA | TOYOTA | VOLKSWAGEN | DRONE
 BRANDS = get_args(Platform)
 
 PLATFORMS: dict[str, Platform] = {str(platform): platform for brand in BRANDS for platform in brand}
