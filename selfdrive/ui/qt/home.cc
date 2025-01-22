a69fa2f9ce6cff20aa5d5ed546c10d08f6d8adae
Kevin Martinez <kevinvr@hotmail.es>
2025-01-21 22:06:29 -0600

    #8 Do not enable robot face when not car config

--- selfdrive/ui/qt/home.cc
+++ selfdrive/ui/qt/home.cc
@@ -61,10 +61,12 @@ void HomeWindow::updateState(const UIState &s) {
   const SubMaster &sm = *(s.sm);
 
   // switch to the generic robot UI
+  /*
   if (onroad->isVisible() && !body->isEnabled() && sm["carParams"].getCarParams().getNotCar()) {
     body->setEnabled(true);
     slayout->setCurrentWidget(body);
   }
+  */
 }
 
 void HomeWindow::offroadTransition(bool offroad) {
