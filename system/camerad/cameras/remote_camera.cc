60a348cd102ab5f0504694c8434f4b660b34ccdf
Kevin Martinez <kevinvr@hotmail.es>
2025-01-28 23:25:11 -0600

    Added vision ipc propper send using opencl

--- system/camerad/cameras/remote_camera.cc
+++ system/camerad/cameras/remote_camera.cc
@@ -1,26 +1,28 @@
 #include "remote_camera.h"
 #include <CL/cl.h>
+#include <arpa/inet.h> // inet_addr()
 #include <cassert>
 #include <cstdint>
 #include <cstdio>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <arpa/inet.h> // inet_addr()
 #include <unistd.h>
 
+extern ExitHandler do_exit;
 
 RemoteCamera::RemoteCamera(std::string addr, uint16_t port, uint16_t height,
-                           uint16_t width, cl_context ctx, cl_device_id device_id,
-                           VisionIpcServer *vipc_server)
+                           uint16_t width, cl_context ctx,
+                           cl_device_id device_id, VisionIpcServer *vipc_server)
     : ip(addr), port(port), height(height), width(width), ctx(ctx),
       vipc_server(vipc_server), last_frame(height * width * 3 / 2, ' ') {
-  (void) this->vipc_server;
+  (void)this->vipc_server;
   cl_int err = 0;
   this->queue = clCreateCommandQueueWithProperties(ctx, device_id, 0, &err);
   assert(err == CL_SUCCESS);
   char cl_arg[1024];
   sprintf(cl_arg,
-          " -DHEIGHT=%d -DWIDTH=%d -DRGB_STRIDE=%d -DUV_WIDTH=%d -DUV_HEIGHT=%d -DRGB_SIZE=%d -DCL_DEBUG ",
+          " -DHEIGHT=%d -DWIDTH=%d -DRGB_STRIDE=%d -DUV_WIDTH=%d "
+          "-DUV_HEIGHT=%d -DRGB_SIZE=%d -DCL_DEBUG ",
           height, width, width * 3, width / 2, height / 2, height * width);
   this->program = cl_program_from_file(
       ctx, device_id, "/data/openpilot/tools/rgb_to_nv12.cl", cl_arg);
@@ -50,19 +52,20 @@ void RemoteCamera::fetch_frame() {
   capnp::PackedFdMessageReader message(sfd);
   cereal::Thumbnail::Reader frame = message.getRoot<cereal::Thumbnail>();
   cl_int err = 0;
-  cl_mem cam_buf_cl =
-      clCreateBuffer(ctx,
-      CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-      width * height * 3,
-      (void*)frame.getThumbnail().begin(), &err);
+  cl_mem cam_buf_cl = clCreateBuffer(
+      ctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, width * height * 3,
+      (void *)frame.getThumbnail().begin(), &err);
   assert(err == CL_SUCCESS);
   clSetKernelArg(kernel, 0, sizeof(cl_mem), &cam_buf_cl);
   clSetKernelArg(kernel, 1, sizeof(cl_mem), &output_cl);
   size_t global_size[] = {static_cast<size_t>(width / 4),
                           static_cast<size_t>(height / 4)};
+  cl_event event;
   err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_size, NULL, 0,
-                               NULL, NULL);
+                               NULL, &event);
   assert(err == CL_SUCCESS);
+  clWaitForEvents(1, &event);
+  assert(clReleaseEvent(event) == CL_SUCCESS);
   clEnqueueReadBuffer(queue, output_cl, CL_TRUE, 0, width * height * 3 / 2,
                       this->last_frame.data(), 0, NULL, NULL);
   clReleaseMemObject(cam_buf_cl);
@@ -70,13 +73,38 @@ void RemoteCamera::fetch_frame() {
 };
 
 void RemoteCamera::fetch_frame_thread() {
-  while (true) {
+  while (!do_exit) {
     this->fetch_frame();
   }
 };
 
+void RemoteCamera::init() {
+  asset(this->vipc_server != nullptr);
+  this->vipc_server->create_buffers(VisionStreamType::VISION_STREAM_ROAD, 5,
+                                    false, this->width, this->height);
+}
+
 void RemoteCamera::run() {
   this->fetch_thread = std::thread(&RemoteCamera::fetch_frame_thread, this);
+  while (!do_exit) {
+    if (!this->recv_frame) {
+      sleep(1);
+      continue;
+    }
+    auto buf =
+        this->vipc_server->get_buffer(VisionStreamType::VISION_STREAM_ROAD);
+    clEnqueueWriteBuffer(queue, buf->cl_buf, CL_TRUE, 0,
+                         this->last_frame.size(),
+                         (void *)this->last_frame.data(), 0, NULL, NULL);
+    VisionIpcBufExtra extra = {
+        this->frame_id,
+        this->frame_id * 0.05 * 1e9,
+        this->frame_id * 0.05 * 1e9,
+    };
+    buf->set_frame_id(this->frame_id);
+    this->vipc_server->send(buf, &extra);
+    this->frame_id++;
+  }
   this->fetch_thread.join();
 };
 
