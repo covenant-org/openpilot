c945059d48d205b885e9da0bc8c0fd283893c62d
Comma Device <device@comma.ai>
2025-02-18 16:49:45 -0600

    #22 Updated angle multiplier
    Will need to revalidate if modification is correct once steringPressed state is implemented correctly

--- launch_hitl.sh
+++ launch_hitl.sh
@@ -2,8 +2,8 @@
 
 export MAVLINK_CONNECTION_URI=udp://:14540
 export REMOTE_CAMERA=${1:-"192.168.100.100:4069"}
-export BLOCK=encoderd,stream_encoderd
+export BLOCK=encoderd,stream_encoderd,dmonitoringd,dmonitoringmodeld
 export DRONE_HEIGHT=${2:-"1.0"}
 export USE_LIVE_CALIBRATION=1
-export USE_AUTOMATIC_CALIBRATION=1
+#export USE_AUTOMATIC_CALIBRATION=1
 exec ./launch_chffrplus.sh
