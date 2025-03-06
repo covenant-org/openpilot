233ef85376743a074f6b45838288e82eac8fa53c
Manuelo247 <manuel8a31@hotmail.com>
2025-03-06 12:56:42 -0600

    Fixed python path

--- tinygrad_repo/openpilot/thneed_models_compile.sh
+++ tinygrad_repo/openpilot/thneed_models_compile.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Ruta del ejecutable de Python
-PYTHON_EXEC="/home/manuelo247/tinygrad/tinygrad_env/bin/python"
+PYTHON_EXEC="/usr/local/pyenv/shims/python3"
 
 # Ruta del script de inferencia
 SCRIPT_PATH="/data/openpilot/tinygrad_repo/openpilot/compile2_nuclea.py"
 
 # Lista de modelos a descargar
 MODELS=(
-    "https://github.com/covenant-org/tinygrad/releases/download/yoloV8-Medium-NucleaV9/best.onnx"
-    # "https://github.com/covenant-org/tinygrad/releases/download/yoloV8-Small-NucleaV9/best.onnx"
-    # "https://github.com/covenant-org/tinygrad/releases/download/yoloV8-Nano-NucleaV9/best.onnx"
+    # "https://github.com/covenant-org/tinygrad/releases/download/yoloV8-Medium-NucleaV9/best.onnx"
+    "https://github.com/covenant-org/tinygrad/releases/download/yoloV8-Small-NucleaV9/best.onnx"
+    "https://github.com/covenant-org/tinygrad/releases/download/yoloV8-Nano-NucleaV9/best.onnx"
 )
 
 for MODEL_URL in "${MODELS[@]}"; do
