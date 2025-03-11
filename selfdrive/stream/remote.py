ad745a7cc2195a99ca0b0a5ff9d3aee9442fcd20
Comma Device <device@comma.ai>
2025-03-11 04:30:18 +0000

    #24 Successful transmission of frames encoded in H264

--- selfdrive/stream/remote.py
+++ selfdrive/stream/remote.py
@@ -4,9 +4,10 @@ import time
 import pickle
 import numpy as np
 import cereal.messaging as messaging
+import socket
+import pickle
 from cereal import car, log
 from pathlib import Path
-import cv2
 from setproctitle import setproctitle
 from cereal.messaging import PubMaster, SubMaster
 from msgq.visionipc import VisionIpcClient, VisionStreamType, VisionBuf
@@ -24,6 +25,12 @@ from openpilot.selfdrive.modeld.parse_model_outputs import Parser
 from openpilot.selfdrive.modeld.fill_model_msg import fill_model_msg, fill_pose_msg, PublishState
 from openpilot.selfdrive.modeld.constants import ModelConstants
 from openpilot.selfdrive.modeld.models.commonmodel_pyx import ModelFrame, CLContext
+import zlib
+from collections import namedtuple
+from threading import Thread
+from time import sleep
+
+FrameInfo = namedtuple("FrameInfo", ["width", "height", "fps", "pixel_format"])
 
 
 PROCESS_NAME = "selfdrive.stream.remote"
@@ -35,6 +42,11 @@ class FrameMeta:
   def __init__(self, vipc=None):
     if vipc is not None:
       self.frame_id, self.timestamp_sof, self.timestamp_eof = vipc.frame_id, vipc.timestamp_sof, vipc.timestamp_eof
+
+def send_frame(conn, frame):
+    frame_raw_nv12 = frame.astype(np.uint8)
+    compressed_frame = zlib.compress(frame_raw_nv12.tobytes())
+    conn.sendall(bytes([1]) + int(len(compressed_frame)).to_bytes(length=4, byteorder="big") + compressed_frame)
 
 
 def main(demo=False):
@@ -55,7 +67,7 @@ def main(demo=False):
 
   vipc_client_main_stream = VisionStreamType.VISION_STREAM_WIDE_ROAD if main_wide_camera else VisionStreamType.VISION_STREAM_ROAD
   vipc_client_main = VisionIpcClient("camerad", vipc_client_main_stream, True, cl_context)
-  cloudlog.warning(f"vision stream set up, main_wide_camera: {main_wide_camera}, use_extra_client: {use_extra_client}")
+  cloudlog.warning(f"vision stream set up, main_wide_camera: {main_wide_camera}")
 
   while not vipc_client_main.connect(False):
     time.sleep(0.1)
@@ -73,62 +85,38 @@ def main(demo=False):
   meta_extra = FrameMeta()
 
   sm = SubMaster(["roadCameraState"])
+  params = Params()
 
   with car.CarParams.from_bytes(params.get("CarParams", block=True)) as msg:
     CP = msg
 
   width, height = vipc_client_main.width, vipc_client_main.height
   fps = 20
-  out = cv2.VideoWriter('appsrc ! videoconvert' + \
-  ' ! video/x-raw,format=I420' + \
-  ' ! x264enc speed-preset=ultrafast bitrate=600 key-int-max=' + str(fps * 2) + \
-  ' ! video/x-h264,profile=baseline' + \
-  ' ! rtspclientsink location=rtsp://159.54.131.60:8554/mystream',
-  cv2.CAP_GSTREAMER, 0, fps, (width, height), True)
+  info = FrameInfo(width=width, height=height, fps=fps, pixel_format="nv12")
+  conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  conn.connect(("192.168.100.100", 4020))
+  serialized_info = pickle.dumps(info)
+#  print(bytes([0])+int(len(serialized_info)).to_bytes(length=4, byteorder="big") + serialized_info)
+  conn.sendall(bytes([0])+int(len(serialized_info)).to_bytes(length=4, byteorder="big") + serialized_info)
+  sock = messaging.sub_sock("livestreamRoadEncodeData", conflate=True)
+
 
   while True:
     # Keep receiving frames until we are at least 1 frame ahead of previous extra frame
-    buf_main = vipc_client_main.recv()
-    meta_main = FrameMeta(vipc_client_main)
-    if buf_main is None:
-      break
 
-    if buf_main is None:
-      cloudlog.debug("vipc_client_main no frame")
-      continue
+    msg = None
+    while True:
+      msg = messaging.recv_one_or_none(sock)
+      if msg is not None:
+        break
+      sleep(0.005)
 
-      if abs(meta_main.timestamp_sof - meta_extra.timestamp_sof) > 10000000:
-        cloudlog.error(f"frames out of sync! main: {meta_main.frame_id} ({meta_main.timestamp_sof / 1e9:.5f}),\
-                         extra: {meta_extra.frame_id} ({meta_extra.timestamp_sof / 1e9:.5f})")
+    evta = getattr(msg, msg.which())
+    frame = evta.header + evta.data
+    conn.sendall(bytes([1]) + int(len(frame)).to_bytes(length=4, byteorder="big") + frame)
 
-    else:
-      # Use single camera
-      buf_extra = buf_main
-      meta_extra = meta_main
+    #send_frame(conn, buf_main.data)
 
-    sm.update(0)
-    frame_id = sm["roadCameraState"].frameId
-    # tracked dropped frames
-    vipc_dropped_frames = max(0, meta_main.frame_id - last_vipc_frame_id - 1)
-    frames_dropped = frame_dropped_filter.update(min(vipc_dropped_frames, 10))
-    if run_count < 10: # let frame drops warm up
-      frame_dropped_filter.x = 0.
-      frames_dropped = 0.
-    run_count = run_count + 1
-
-    frame_drop_ratio = frames_dropped / (1 + frames_dropped)
-    prepare_only = vipc_dropped_frames > 0
-    if prepare_only:
-      cloudlog.error(f"skipping remote frame. Dropped {vipc_dropped_frames} frames")
-
-    mt1 = time.perf_counter()
-    frame_raw_nv12 = buf_main.data
-    frame_rgb = cv2.cvtColor(frame_raw_nv12, cv2.COLOR_YUV2RGB_YV12)
-    out.write(frame_rgb)
-    mt2 = time.perf_counter()
-    model_execution_time = mt2 - mt1
-
-    last_vipc_frame_id = meta_main.frame_id
 
 
 if __name__ == "__main__":
